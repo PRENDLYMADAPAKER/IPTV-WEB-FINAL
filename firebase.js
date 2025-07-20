abba16aa8e4b617b77e5eab1d3592c010a750520
PRENDLYMADAPAKER <palasa1919@gmail.com>
2025-07-20 19:14:20 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyA0TjMoFSYBIs0VQ9shUilOuDGb1uXHjKI",
   authDomain: "iptv-log-in.firebaseapp.com",
   projectId: "iptv-log-in",
-  storageBucket: "iptv-log-in.firebasestorage.app",
+  storageBucket: "iptv-log-in.appspot.com",
   messagingSenderId: "820026131349",
   appId: "1:820026131349:web:417abd6ad9057c55a92c9c",
-  measurementId: "G-4Y8T6J595Z",
-  databaseURL: "https://iptv-log-in-default-rtdb.firebaseio.com/"
+  measurementId: "G-4Y8T6J595Z"
 };
 
 firebase.initializeApp(firebaseConfig);
